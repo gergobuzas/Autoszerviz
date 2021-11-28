3e5de424c23b2b80f246627f524cc3919ca10359
gergobuzas <buzasgergo0615@gmail.com>
2021-11-28 21:01:11 +0100

    Create DependInfo.cmake

---
+++ cmake-build-debug/CMakeFiles/Autoszerviz.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/Egyetem/Prog/NagyHF/Autoszerviz_codeblocks/Autoszerviz/database.c" "D:/Egyetem/Prog/NagyHF/Autoszerviz_codeblocks/Autoszerviz/cmake-build-debug/CMakeFiles/Autoszerviz.dir/database.c.obj"
+  "D:/Egyetem/Prog/NagyHF/Autoszerviz_codeblocks/Autoszerviz/econio.c" "D:/Egyetem/Prog/NagyHF/Autoszerviz_codeblocks/Autoszerviz/cmake-build-debug/CMakeFiles/Autoszerviz.dir/econio.c.obj"
+  "D:/Egyetem/Prog/NagyHF/Autoszerviz_codeblocks/Autoszerviz/functions.c" "D:/Egyetem/Prog/NagyHF/Autoszerviz_codeblocks/Autoszerviz/cmake-build-debug/CMakeFiles/Autoszerviz.dir/functions.c.obj"
+  "D:/Egyetem/Prog/NagyHF/Autoszerviz_codeblocks/Autoszerviz/main.c" "D:/Egyetem/Prog/NagyHF/Autoszerviz_codeblocks/Autoszerviz/cmake-build-debug/CMakeFiles/Autoszerviz.dir/main.c.obj"
+  "D:/Egyetem/Prog/NagyHF/Autoszerviz_codeblocks/Autoszerviz/menu.c" "D:/Egyetem/Prog/NagyHF/Autoszerviz_codeblocks/Autoszerviz/cmake-build-debug/CMakeFiles/Autoszerviz.dir/menu.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
