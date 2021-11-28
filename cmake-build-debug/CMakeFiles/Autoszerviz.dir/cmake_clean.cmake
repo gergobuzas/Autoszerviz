d50e24dc193651456e59f7a9970322b779791e22
gergobuzas <buzasgergo0615@gmail.com>
2021-11-28 21:00:51 +0100

    Create cmake_clean.cmake

---
+++ cmake-build-debug/CMakeFiles/Autoszerviz.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "Autoszerviz.exe"
+  "Autoszerviz.exe.manifest"
+  "Autoszerviz.pdb"
+  "CMakeFiles/Autoszerviz.dir/database.c.obj"
+  "CMakeFiles/Autoszerviz.dir/econio.c.obj"
+  "CMakeFiles/Autoszerviz.dir/functions.c.obj"
+  "CMakeFiles/Autoszerviz.dir/main.c.obj"
+  "CMakeFiles/Autoszerviz.dir/menu.c.obj"
+  "libAutoszerviz.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Autoszerviz.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
